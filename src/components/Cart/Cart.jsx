34a3c5617f5fec6d31629ad842f73799a3443998
automotorLeonardoMannanice <lic.mannanice@gmail.com>
2022-03-01 20:32:18 -0300

    Id orden

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,51 +1,9 @@
-import { getFirestore,collection, addDoc,doc, updateDoc, query, where, documentId, writeBatch, getDocs } from "firebase/firestore";
+
 import { useCartContext } from "../../Context/CartContext";
 import ComponentForm from "../ComponentForm/componentForm";
 const Cart = () => {
-const {cartList,vaciarCarrito,sumaTotal,borrarItem}=useCartContext();
-
-const buy = async (e)=>{
-    e.preventDefault();
-    let purchaseOrder   = {}
-    purchaseOrder.buyer = {name:'Leonardo Mannanice',email:'lic.mannanice@gmail.com',phone:'2257516706'}    
-    purchaseOrder.total = sumaTotal()
-    
-    purchaseOrder.items = cartList.map(cartItem => {
-      const id    = cartItem.item.id;
-      const name  = cartItem.item.name;
-      const price = cartItem.item.price;
-      const cant  = cartItem.cantidad;
-      return{
-        id,
-        name,
-        price,
-        cant
-      }
-    })
-   
-    const db = getFirestore()
-    const queryCollection = collection(db,'items')
-    const queryUpdateStock = query(
-      queryCollection,
-      where (documentId(),'in',cartList.map(it => it.item.id))
-    )
-    const batch = writeBatch(db)
-     
-    await getDocs(queryUpdateStock)
-    .then(resp => resp.docs.forEach(res => batch.update(res.ref,{
-          stock :res.data().stock - cartList.find(item=>item.item.id === res.id).cantidad
-          })
-    ))
-    .catch(err => console.log(err))
-    .finally(() =>
-        alert('ORDEN GENERADO CON ÉXITO'), 
-        vaciarCarrito())
-    batch.commit()
-
-
-
-
-}
+  const {cartList,vaciarCarrito,sumaTotal,borrarItem}=useCartContext();
+ 
   return <div>
           {cartList.length !== 0 ?
             <>
@@ -57,7 +15,7 @@ const buy = async (e)=>{
                         </div> )}
            
               <ComponentForm />
-              {<button onClick={buy}>Crear orden</button>}              
+              {/*<button onClick={buy}>Crear orden</button>*/}              
               
               {`suma ${sumaTotal()}`}
               {<button onClick={vaciarCarrito}>Limpiar</button>}              
--- src/components/ComponentForm/componentForm.jsx
+++ src/components/ComponentForm/componentForm.jsx
@@ -1,7 +1,60 @@
 import React, { useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
-
+import { getFirestore,collection, addDoc,doc, updateDoc, query, where, documentId, writeBatch, getDocs } from "firebase/firestore";
+import { useCartContext } from '../../Context/CartContext';
 const ComponentForm = () => {
+    const {cartList,vaciarCarrito,sumaTotal,borrarItem}=useCartContext();
+
+    const [idOrder, setIdOrder] = useState('')
+  
+    const buy = async (e)=>{
+        e.preventDefault();
+        
+        let purchaseOrder   = {}
+        purchaseOrder.buyer = dataForm    
+        purchaseOrder.total = sumaTotal()
+        
+        purchaseOrder.items = cartList.map(cartItem => {
+            const id    = cartItem.item.id;
+            const name  = cartItem.item.name;
+            const price = cartItem.item.price;
+            const cant  = cartItem.cantidad;
+            return{
+                id,
+                name,
+                price,
+                cant
+            }
+        })
+       
+        const db = getFirestore()
+        const ordersCollection = collection (db, 'orders')
+       
+        await addDoc( ordersCollection, purchaseOrder)
+        .then(resp => setIdOrder(resp.id) )
+       
+        console.log("el idOrder es:")
+        
+        console.log(idOrder)
+
+        const queryCollection = collection(db,'items')
+        const queryUpdateStock = query(
+          queryCollection,
+          where (documentId(),'in',cartList.map(it => it.item.id))
+        )
+        const batch = writeBatch(db)
+       
+        await getDocs(queryUpdateStock)
+        .then(resp => resp.docs.forEach(res => batch.update(res.ref,{
+              stock:res.data().stock - cartList.find(item=>item.item.id === res.id).cantidad
+              })
+        ))
+        .catch(err => console.log(err))
+        .finally(() =>
+            alert('ORDEN GENERADO CON ÉXITO'), 
+            vaciarCarrito())
+        batch.commit()
+  }
     const[dataForm , setDataForm] = useState({
         name   : '',
         dni    : '',
@@ -17,15 +70,10 @@ const ComponentForm = () => {
         })
       
         console.log(dataForm);
-
-
     }
-    const buy1 =async(e)=>{
-        e.preventDefault()
-
-    }
+   
   return (
-     <Form onSubmit={buy1}>
+     <Form onSubmit={buy}>
         <Form.Group className="mb-3" controlId="formName">
             <Form.Label>NOMBRE</Form.Label>
             <Form.Control 
